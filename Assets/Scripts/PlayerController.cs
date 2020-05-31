5ff63d00d16fb064547fea65130c8bfef743b43b
MikoLovesGames <Wenxiao0015@icloud.com>
2020-05-31 10:53:21 +0800

    第一节课内容，Rig2d组建 AddForce、 GravityScale ，自定义组件，FixUpdate， Keydown，KeyUp

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator AnimatorComp;
+    private Rigidbody2D Rigidbody2DComp;
+    private bool bGameStart = false;
+
+    public float MoveSpeed = .02f;
+    public float JumpForce = 300;
+    public float GravityNormal = 3.0f;
+    public float GravityJump = 1.2f;
+
+    private void Awake()
+    {
+        AnimatorComp = GetComponent<Animator>();
+        Rigidbody2DComp = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        JumpOff();
+    }
+
+    private void FixedUpdate()
+    {
+        if (bGameStart)
+        {
+            transform.Translate(new Vector2(MoveSpeed, .0f));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            JumpOff();
+        }
+    }
+
+    public void Play()
+    {
+        AnimatorComp.SetTrigger("WalkTrigger");
+        bGameStart = true;
+    }
+
+    public void Jump()
+    {
+        Rigidbody2DComp.gravityScale = GravityJump;
+        Rigidbody2DComp.AddForce(new Vector2(0, JumpForce));
+    }
+
+    public void JumpOff()
+    {
+        Rigidbody2DComp.gravityScale = GravityNormal;
+    }
+}
